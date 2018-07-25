22b810e56fd1659723e487699877e94f2dfa3bd4
Adriana Disano <adrianadisano@gmail.com>
2018-07-25 02:08:40 -0500

    Created

--- MultiplyLoop/Program.cs
+++ MultiplyLoop/Program.cs
@@ -6,7 +6,21 @@ namespace MultiplyLoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number:");
+            string userInputStr = Console.ReadLine();
+            int userInputInt = int.Parse(userInputStr);
+
+            int sumOutput = 0;
+
+            for (int loopStart = 2; loopStart < 20; loopStart++)
+            {
+                Console.WriteLine(loopStart * userInputInt);
+
+                sumOutput += loopStart * userInputInt;
+            }
+
+            Console.Write(sumOutput);
+            Console.ReadLine();
         }
     }
 }
