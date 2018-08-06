8e28fa00b35387d4144c9d322b00a3bd0aa70b6e
Adriana Disano <adrianadisano@gmail.com>
2018-08-06 16:07:18 -0500

    Fixed a bug where the program was looping from 2 to 19.

--- MultiplyLoop/Program.cs
+++ MultiplyLoop/Program.cs
@@ -6,20 +6,30 @@ namespace MultiplyLoop
     {
         static void Main(string[] args)
         {
+            //prompt user for input
             Console.WriteLine("Please enter a number:");
+            //acquire user input
             string userInputStr = Console.ReadLine();
+            //convert user input to integer
             int userInputInt = int.Parse(userInputStr);
 
+
+            //declare an integer and assign it a value that will be changed later
             int sumOutput = 0;
 
-            for (int loopStart = 2; loopStart < 20; loopStart++)
+            //loop from 2 to 20, incrementing loopStart each time
+            for (int loopStart = 2; loopStart < 21; loopStart++)
             {
+                //display the result of loopStart * userInputInt
                 Console.WriteLine(loopStart * userInputInt);
 
+                //add the result of loopstart * userInputInt to the variable sumOutput
                 sumOutput += loopStart * userInputInt;
             }
 
+            //display SumOutput
             Console.Write(sumOutput);
+            //wait for user input
             Console.ReadLine();
         }
     }
